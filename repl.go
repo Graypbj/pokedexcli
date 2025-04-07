3dd2c095b54aec62519ffe3718e96fef64c2b5db
Graypbj <graypbj@gmail.com>
2025-04-07 11:27:35 -0600

    Added cli commands

--- repl.go
+++ repl.go
@@ -18,9 +18,14 @@ func startRepl() {
 			continue
 		}
 
-		commandNames := words[0]
+		commandName := words[0]
 
-		fmt.Printf("Your command was: %s\n", commandNames)
+		command, exists := getCommands()[commandName]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback()
 	}
 }
 
@@ -28,4 +33,25 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
